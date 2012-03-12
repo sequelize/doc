acad22b9efc4e2df50d3c5a42ff91419fbe6e186
sdepold <sascha@depold.com>
2012-03-12 18:13:51 +0100

    first steps for new express usage

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express')
-var app = module.exports = express.createServer()
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
 
 // Configuration
+
 app.configure(function(){
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'ejs')
-  app.helpers(require("express-view-helpers"))
-  app.helpers({
-    koala: require("koala").render
-  })
-  app.use(express.bodyParser())
-  app.use(express.methodOverride())
-  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }))
-  app.use(app.router)
-  app.use(express.static(__dirname + '/public'))
-})
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
-})
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.configure('production', function(){
-  app.use(express.errorHandler())
-})
+  app.use(express.errorHandler());
+});
 
 // Routes
-app.get('/', function(req, res){
-  var navigation = {
-    "installation": 'Installation',
-    "usage": 'Usage',
-    "sync-with-db": 'Synchronize with database',
-    "instances": "Models and instances",
-    "expanding-models": "Expanding models",
-    "chain-queries": "Chain queries",
-    "associations": "Associations",
-    "find-objects": "Finding objects",
-    "dialects": 'SQL dialects',
-    "migrations": 'Migrations',
-    'additional': 'Additional information',
-    "projects": "Sequelize-based projects"
-  }
 
-  res.render('index', {
-    navigation: navigation,
-    active: req.param('active') || 'installation'
-  })
-})
+app.get('/', routes.index);
 
-if (!module.parent) {
-  app.listen(process.env.PORT || 3000)
-  console.log("Express server listening on port %d", app.address().port)
-}
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
