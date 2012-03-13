93ee758365ff2b4e3a45e98a581fc460398ca516
sdepold <sascha@depold.com>
2012-03-13 13:36:03 +0100

    first draft of new bootstrap ui is working

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes');
+const express = require('express')
 
 var app = module.exports = express.createServer();
 
@@ -29,7 +23,20 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Sequelize &raquo; A MySQL Object-Relational-Mapper for NodeJS',
+    sections: [
+      'Installation',
+      'Usage',
+      'DAOFactory',
+      'DAO',
+      'Migrations',
+      'Further information',
+      'Projects'
+    ]
+  })
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
