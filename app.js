dec273729f29c3390be696baf9c9ad735ab21d92
sdepold <sascha@depold.com>
2012-04-25 16:40:35 +0200

    allowed overriding of app port

--- app.js
+++ app.js
@@ -42,5 +42,5 @@ app.get('/', function(req, res){
   })
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || process.env.port || 3000)
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
