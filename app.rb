ca374c925a5f6f47a21688d24619e1bd5203e530
Sascha Depold <sascha@depold.com>
2013-04-30 23:27:07 +0200

    first dummy md file is done

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'redcarpet'
+
+class SequelizeRenderer < Redcarpet::Render::HTML
+  def header(text, header_level)
+    result = "<h#{header_level}>#{text}</h#{header_level}>"
+
+    if header_level == 2
+      result = "<div class=\"page-header\">#{result}</div>"
+    end
+
+    result
+  end
+end
+
+set :markdown, :renderer => SequelizeRenderer, :fenced_code_blocks => true
 
 get '/' do
+  def markdown
+  Redcarpet::Markdown.new(SequelizeRenderer, :fenced_code_blocks => true)
+end
+
   erb 'documentation/index'.to_sym
 end
