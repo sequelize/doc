689f1b83e5e976287db2ba1f97605a904a140171
Sascha Depold <sascha@depold.com>
2013-05-03 22:30:51 +0200

    changed headline parser

--- lib/sequelize_renderer.rb
+++ lib/sequelize_renderer.rb
@@ -1,4 +1,10 @@
 class SequelizeRenderer < Redcarpet::Render::HTML
+  def header(text, level)
+    display_value, nav_value = text.split('|').map(&:strip)
+    attrs = (nav_value ? " data-nav-value='#{nav_value}'" : "")
+    "<h#{level}#{attrs}>#{display_value}</h#{level}>"
+  end
+
   def doc_header
     '<div class="row"><div class="span12">'
   end
