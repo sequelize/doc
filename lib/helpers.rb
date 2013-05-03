8c5397b210fe95fc484b51fccde897120db3c0f3
Sascha Depold <sascha@depold.com>
2013-05-03 22:29:57 +0200

    fixed navigation structure

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -13,33 +13,37 @@ class Helpers
       if section.css('h3').size == 0
         nav_content += navigation_item(section_id, section_name)
       else
-        sub_nav = '<div class="subnav"><ul class="nav nav-pills">'
-        items   = []
+        sub_nav   = '<div class="subnav"><ul class="nav nav-pills">'
+        sub_items = []
+        nav_items = []
 
         section.css('h3').each do |h3|
-          h3_id = "#{section_id}-#{h3.content.downcase.gsub(/\W/, '-')}"
+          h3_value = h3["data-nav-value"] || h3.content
+          h3_id    = "#{section_id}-#{h3_value.downcase.gsub(/\W/, '-')}"
           h3.parent.parent["id"] = h3_id
 
-          items << { :identifier => h3_id, :label => h3.content }
+          nav_items << { :identifier => h3_id, :label => h3_value }
+          sub_items << { :identifier => h3_id, :label => h3_value }
 
           if h3.parent.css('h4').size == 0
-            sub_nav += navigation_item(h3_id, h3.content)
+            sub_nav += navigation_item(h3_id, h3_value)
           else
-            items = h3.parent.css('h4').map do |h4|
-              h4_id    = "#{h3_id}-#{h4.content.downcase.gsub(/\W/, '-')}"
+            sub_items = h3.parent.css('h4').map do |h4|
+              h4_value = h4["data-nav-value"] || h4.content
+              h4_id    = "#{h3_id}-#{h4_value.downcase.gsub(/\W/, '-')}"
               h4["id"] = h4_id
 
-              { :identifier => h4_id, :label => h4.content }
+              { :identifier => h4_id, :label => h4_value }
             end
 
-            sub_nav += navigation_group(h3.content, items)
+            sub_nav += navigation_group(h3_value, sub_items)
           end
         end
 
         sub_nav += "</ul></div>"
 
         section.at_css('.page-header').inner_html += sub_nav
-        nav_content += navigation_group(section_name, items)
+        nav_content += navigation_group(section_name, nav_items)
       end
     end
 
@@ -64,7 +68,9 @@ private
         <ul class="dropdown-menu">
     HTML
 
-    items.each{ |item| html += navigation_item(item[:identifer], item[:label]) }
+    items.each do |item|
+      html += navigation_item(item[:identifier], item[:label])
+    end
 
     html += <<-HTML
         </ul>
