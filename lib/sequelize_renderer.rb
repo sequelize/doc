5678896e673caf23a84d9870ab623c3f24ba143d
Sascha Depold <sascha@depold.com>
2013-05-04 14:40:39 +0200

    styling

--- lib/sequelize_renderer.rb
+++ lib/sequelize_renderer.rb
@@ -1,8 +1,15 @@
 class SequelizeRenderer < Redcarpet::Render::HTML
   def header(text, level)
     display_value, nav_value = text.split('|').map(&:strip)
-    attrs = (nav_value ? " data-nav-value='#{nav_value}'" : "")
-    "<h#{level}#{attrs}>#{display_value}</h#{level}>"
+
+    attrs  = (nav_value ? " data-nav-value='#{nav_value}'" : "")
+    result = "<h#{level}#{attrs}>#{display_value}</h#{level}>"
+
+    if level == 2
+      result = "<div class='page-header'>#{result}</div>"
+    end
+
+    result
   end
 
   def doc_header
