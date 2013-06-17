6b5b3f0b43c932ec3f5f54ee8eca388dbf07cc15
Sascha Depold <sascha@depold.com>
2013-06-17 20:18:11 +0200

    use rack rewrite to fix the url

--- app.rb
+++ app.rb
@@ -8,33 +8,17 @@ require './lib/helpers'
 
 set :markdown, :renderer => SequelizeRenderer, :fenced_code_blocks => true, :strikethrough => true
 
-def check_host(req)
-  uri = URI(req.url)
-
-  allowed_hosts = [/^sequelizejs.com/, /^localhost/]
-
-  unless allowed_hosts.map{ |host| !!uri.host.match(host) }.include?(true)
-    uri.host = 'sequelizejs.com'
-    redirect to(uri.to_s), 301
-  end
-end
-
 get '/' do
-  check_host(request)
   redirect to('/documentation')
 end
 
 get '/documentation' do
-  check_host(request)
-
   html = erb('documentation/index'.to_sym)
   html = Helpers.inject_navigation(html)
   html
 end
 
 get '/heroku' do
-  check_host(request)
-
   html = erb('articles/heroku/index'.to_sym)
   html = Helpers.inject_navigation(html)
 
@@ -42,8 +26,6 @@ get '/heroku' do
 end
 
 get '/changelog' do
-  check_host(request)
-
   changelog = `curl https://raw.github.com/sequelize/sequelize/master/changelog.md`
   changelog = changelog.gsub('# ', '### ').scan(/(###.+?)\n\n/m)
 
@@ -54,10 +36,13 @@ get '/changelog' do
 end
 
 get '/imprint' do
-  check_host(request)
-
   html = erb('imprint/index'.to_sym)
   html = Helpers.inject_navigation(html)
 
   html
 end
+
+get '/blog' do
+
+end
+
--- config.ru
+++ config.ru
@@ -1,2 +1,10 @@
+require 'rack/rewrite'
+
+use Rack::Rewrite do
+  r301 %r{.*}, 'http://sequelizejs.com$&', :if => Proc.new { |rack_env|
+    (rack_env['SERVER_NAME'] != 'sequelizejs.com') && !rack_env['SERVER_NAME'].include?('localhost')
+  }
+end
+
 require './app'
 run Sinatra::Application
