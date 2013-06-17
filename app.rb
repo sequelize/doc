5ab3fa82d348cdf19e910c60b473fc76600ace23
Sascha Depold <sascha@depold.com>
2013-06-17 21:46:48 +0200

    blog posts

--- app.rb
+++ app.rb
@@ -43,6 +43,28 @@ get '/imprint' do
 end
 
 get '/blog' do
+  posts = Dir['views/blog/posts/*md'].map do |d|
+    d.sub('views/blog/posts/', '').sub('.md', '')
+  end.sort_by(&:to_i).map do |filename|
+    html = markdown("blog/posts/#{filename}".to_sym)
 
+    [
+      filename, {
+        :title   => [html.match(/<h3>(.*)<\/h3>/), $1].last,
+        :content => html
+      }
+    ]
+  end.reverse
+
+  html = erb('blog/index'.to_sym, :locals => { :posts => posts })
 end
 
+get '/blog/:title' do
+  post = Dir['views/blog/posts/*md'].map do |d|
+    d.sub('views/blog/posts/', '').sub('.md', '')
+  end.detect do |d|
+    d.sub("#{d.to_i}-", "") == params[:title]
+  end
+
+  html = erb('blog/show'.to_sym, :locals => { :post => post })
+end
--- config.ru
+++ config.ru
@@ -4,6 +4,7 @@ use Rack::Rewrite do
   r301 %r{.*}, 'http://sequelizejs.com$&', :if => Proc.new { |rack_env|
     (rack_env['SERVER_NAME'] != 'sequelizejs.com') && !rack_env['SERVER_NAME'].include?('localhost')
   }
+  r301 '/post/23227764351/changes-in-sequelize-1-2-1', '/blog/changes-in-sequelize-v1-2-1'
 end
 
 require './app'
