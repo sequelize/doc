9dd7071deb2f0674820554bd05500c80ce5f10d3
Sascha Depold <sascha@depold.com>
2013-11-22 15:12:50 +0100

    made it possible to load different sections

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express')
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
-app.use(express.favicon())
+app.use(express.favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded())
@@ -24,8 +24,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index)
-app.get('/docs', function(req, res) { res.redirect("/docs/latest", 301) })
+app.get('/docs', docs.index)
 app.get('/docs/:version', docs.index)
+app.get('/docs/:version/:section', docs.index)
+app.get('/docs/:version/:section/:subsection', docs.index)
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'))
--- routes/docs.js
+++ routes/docs.js
@@ -1,9 +1,15 @@
-exports.index = function(req, res) {
-  var path = 'docs/latest/index'
+var sections = ["installation", "usage", "models", "instances", "associations", "migrations", "utils", "misc"]
 
-  if (req.param('version') !== 'latest') {
-    path = 'docs/' + req.param('version') + '/views/docs/latest/index'
+exports.index = function(req, res) {
+  var path = 'docs/latest'
+
+  if (!req.param('version')) {
+    res.redirect("/docs/latest", 301)
+  } else if (!req.param('section')) {
+    res.redirect("/docs/latest/" + sections[0], 301)
+  } else if (req.param('version') !== 'latest') {
+    path = 'docs/' + req.param('version') + '/views/' + path
   }
 
-  res.render(path, { title: 'Documentation' })
+  res.render(path + '/' + req.param('section'), { title: 'Documentation' })
 }
