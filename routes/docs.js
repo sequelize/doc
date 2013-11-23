8ca19762f4a93447da7895507fba4d2e6ef5be30
Sascha Depold <sascha@depold.com>
2013-11-23 22:10:43 +0100

    added sub items to navigations

--- routes/docs.js
+++ routes/docs.js
@@ -1,7 +1,32 @@
-var sections = ["installation", "usage", "models", "instances", "associations", "migrations", "utils", "misc"]
+var fs           = require('fs')
+  , _            = require('lodash')
+  , sectionNames = ["installation", "usage", "models", "instances", "associations", "migrations", "utils", "misc"]
+
+var readSubSections = function(path) {
+  try {
+    var content = fs.readFileSync(__dirname + '/../views/' + path + '.jade').toString()
+      , result  = []
+
+    content.split('\n').forEach(function(line) {
+      if (_.contains(line, 'h3')) {
+        var fragments = line.match(/h3#([^\s]+)\s(.+)/)
+        result.push({
+          anchor: fragments[1],
+          text:   fragments[2]
+        })
+      }
+    })
+
+    return result
+  } catch(e) {
+    console.log('Unknown file: ', path)
+    return []
+  }
+}
 
 exports.index = function(req, res) {
-  var path = 'docs/latest'
+  var path     = 'docs/latest'
+    , sections = {}
 
   if (!req.param('version')) {
     res.redirect("/docs/latest", 301)
@@ -10,6 +35,12 @@ exports.index = function(req, res) {
   } else if (req.param('version') !== 'latest') {
     path = 'docs/.' + req.param('version') + '/views/' + path
   }
+
+  sectionNames.forEach(function(section) {
+    sections[section] = readSubSections(path + '/' + section)
+  })
+
+  console.log(sections)
 
   res.render(path + '/' + req.param('section'), {
     title:         'Documentation',
