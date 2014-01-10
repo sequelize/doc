ec24aada72b29566beda94bc73c1a7077a7371b1
Sascha Depold <sascha@depold.com>
2014-01-10 08:51:05 +0100

    fixed redirects

--- routes/docs.js
+++ routes/docs.js
@@ -10,9 +10,9 @@ exports.index = function(req, res) {
     , sections = []
 
   if (!req.param('version')) {
-    res.redirect("/docs/latest", 301)
+    return res.redirect("/docs/latest", 301)
   } else if (!req.param('section')) {
-    res.redirect("/docs/" + req.param('version') + "/" + sectionNames[0], 301)
+    return res.redirect("/docs/" + req.param('version') + "/" + sectionNames[0], 301)
   } else if (req.param('version') !== 'latest') {
     path = 'docs/.' + req.param('version') + '/views/' + path
   }
