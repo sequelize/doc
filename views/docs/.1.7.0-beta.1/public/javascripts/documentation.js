4c5e477970cbac57bdab17fd3a8667ea2101d3ec
Sascha Depold <sascha@depold.com>
2014-05-30 22:03:46 +0200

    test

--- views/docs/.1.7.0-beta.1/public/javascripts/documentation.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('.mini-browser').each(function() {
-    var $browser = $(this)
-
-    $browser
-      .append($('<img src="/images/users/mini-browser.png">'))
-      .css('background', 'transparent url(' + $browser.data('img-url') + ') no-repeat left 42px')
-  })
-})
