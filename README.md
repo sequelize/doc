e09dae710b49acf097089b2f25c41ae15f664d23
Sushant <sushantdhiman@outlook.com>
2019-07-20 20:18:11 +0530

    change: enable documentation deploy

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+### v5 docs:
+https://sequelize.github.io/master
+
 ### v4 docs:
-http://docs.sequelizejs.com
+https://sequelize.github.io/v4
 
 ### v3 docs:
-http://sequelize.readthedocs.io/en/v3/
+https://sequelize.github.io/v3
---
+++ build.sh
@@ -0,0 +1,27 @@
+CLONE_URL=https://github.com/sequelize/sequelize.git
+REPO_NAME=sequelize
+BUILD_DIR=site
+
+build_branch () {
+  git checkout $1
+
+	npm install
+	npm run docs
+
+  rm -rf ../$1
+  if [ $1 == "v3" ];then
+    mkdocs build --verbose --clean --strict
+    mv ./docs ../$BUILD_DIR/$1
+  else
+    mv ./esdoc ../$BUILD_DIR/$1
+  fi
+}
+
+rm -rf $REPO_NAME
+git clone $CLONE_URL
+
+cd $REPO_NAME
+
+build_branch master
+build_branch v4
+build_branch v3
